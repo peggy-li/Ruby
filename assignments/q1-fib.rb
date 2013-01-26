0c0870577285159ee9548a56915926d0d0c30815
peggy-li <peggy.li@duke.edu>
2013-01-25 23:06:59 -0500

    Added solution to q1

--- assignments/q1-fib.rb
+++ assignments/q1-fib.rb
@@ -1,3 +1,7 @@
 def fib(n, n1 = 1, n2 = 1)
-  # ...
+  return n1 if n == 1;
+  return n2 if n == 2;
+
+  # puts "n = #{n}, calling f(#{n-1}) and f(#{n-2})"
+  return fib(n-1, n1, n2) + fib(n-2, n1, n2)
 end
