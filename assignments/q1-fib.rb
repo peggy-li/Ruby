9ecadc7e5679e340766e80eaff23dcc91ff79d9d
peggy-li <peggy.li@duke.edu>
2013-01-31 17:32:24 -0500

    Changed to iterative solution

--- assignments/q1-fib.rb
+++ assignments/q1-fib.rb
@@ -1,7 +1,15 @@
 def fib(n, n1 = 1, n2 = 1)
-  return n1 if n == 1;
-  return n2 if n == 2;
+  # return n1 if n == 1;
+  # return n2 if n == 2;
 
-  # puts "n = #{n}, calling f(#{n-1}) and f(#{n-2})"
-  return fib(n-1, n1, n2) + fib(n-2, n1, n2)
+  # # puts "n = #{n}, calling f(#{n-1}) and f(#{n-2})"
+  # return fib(n-1, n1, n2) + fib(n-2, n1, n2)
+
+  a = Array.new()
+  a[0] = n1
+  a[1] = n2
+  for i in 2..n
+  	a[i] = a[i-1] + a[i-2]
+  end
+  return a[n-1]
 end
