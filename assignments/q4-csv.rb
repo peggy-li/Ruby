daadcc589f312b3a18ba4dd50b6bf677d671d7b5
peggy-li <peggy.li@duke.edu>
2013-01-31 17:22:49 -0500

    Added solution to q4

--- assignments/q4-csv.rb
+++ assignments/q4-csv.rb
@@ -1,3 +1,9 @@
+=begin
+Modify the CSV application to support an each method to return a CsvRow object.
+Use method_missing on that CsvRow to return the value for the column for a given heading.
+=end
+
+
 module ActsAsCsv
   def self.included(base)
     base.extend ClassMethods
@@ -22,5 +28,43 @@ end
 class RubyCsv
   include ActsAsCsv
   acts_as_csv
+
+  def each(&block)
+    @csv_contents.each do |row|
+      block.call(CsvRow.new(row, @headers))
+    end
+  end
 end
 
+class CsvRow
+  attr_accessor :row, :header
+  def initialize(row, header)
+    @row = row
+    @header = header
+  end
+
+  # What should arguments be?
+  def method_missing(name, *args)
+    puts "Method missing\t\"#{name}\""
+
+    # Find index i of header in header
+    i = @header.index(name.to_s)
+    # puts "Header\t#{@header.join(',')}"
+    # puts "#{name} at index #{i}"
+
+    # Print row[i] for row in contents
+    return row[i]
+      
+  end
+end
+
+
+# lines = [
+#   'one, two',
+#   'lions, tigers'
+# ]
+# csv = RubyCsv.new(lines)
+# csv.each {|row| puts row.one}
+
+#  http://stackoverflow.com/questions/5739900/ruby-blocks-with-method-missing
+#  http://www.alef1.org/ruby/method_missing/
