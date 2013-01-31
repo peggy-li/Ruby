334ceffa377de94dcd48d05c660f82d6f84c52fa
peggy-li <peggy.li@duke.edu>
2013-01-31 16:59:36 -0500

    Added solution to q3

--- assignments/q3-tree.rb
+++ assignments/q3-tree.rb
@@ -2,8 +2,17 @@ class Tree
   attr_accessor :children, :node_name
 
   def initialize(name, children=[])
+    
+    @node_name = name.keys[0]
     @children = children
-    @node_name = name
+
+    name[@node_name].each do |key, val|
+      # puts "#{key}, #{val}"
+      # puts "#{val} is #{val.class}"
+      children.push(Tree.new({key => val})) unless val.empty?
+    end
+
+    # puts "name: #{node_name} -> children: #{children}"
   end
 
   def visit_all(&block)
@@ -15,3 +24,22 @@ class Tree
     block.call self
   end
 end
+
+# tree = Tree.new({
+#   'grandpa' => {
+#     'dad' => {
+#       'child 1' => {},
+#       'child 2' => {}
+#     },
+#     'uncle' => {
+#       'child 3' => {}, 
+#       'child 4' => {}
+#     }
+#   }
+# })
+
+# puts "Visit single node"
+# puts tree.visit{|node| puts node.node_name}
+
+# puts "visit tree"
+# tree.visit_all {|node| puts node.node_name}
